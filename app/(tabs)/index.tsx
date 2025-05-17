c80c7ce30dfd0bdf0caf88b1c1132fffa87ecdc4
Shristi Khadka <shristikhadka0988@gmail.com>
2025-05-17 15:59:40 -0400

    add search button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,25 @@
 
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { ScrollView, Text, View } from "react-native";
+import { Image } from 'react-native';
+import { SearchBar } from "react-native-screens";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className ="text-5xl text-dark-200 font-bold">Welcome!</Text>
-    </View>
+
+   <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+        <ScrollView className="flex-1 px-5"
+        showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%",paddingBottom:10}}
+        >
+          <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        </ScrollView>
+
+        <View className="flex-1 mt-5">
+          <SearchBar/>
+        </View>
+
+   </View>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text,Image } from 'react-native';
+import{icons} from "@/constants/icons";
+
+const SearchBar = () => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff"/>
+    </View>
+  );
+};
+
+export default SearchBar;
